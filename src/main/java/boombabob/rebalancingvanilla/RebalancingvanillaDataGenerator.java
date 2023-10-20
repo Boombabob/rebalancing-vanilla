a682c614ffe04eee807638ebdea60006e7fa1e17
james <james@mazuma.net.au>
2023-10-21 09:54:18 +1300

    Initial Commit

---
+++ src/main/java/boombabob/rebalancingvanilla/RebalancingvanillaDataGenerator.java
@@ -0,0 +1,11 @@
+package boombabob.rebalancingvanilla;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RebalancingvanillaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
