76b47a8cc45d1ca6c8601cbefb1d0de0d7a41f88
james <james@mazuma.net.au>
2023-11-02 10:30:49 +1300

    Remove unused import

--- src/main/java/boombabob/rebalancingVanilla/mixin/OreDropsMixin.java
+++ src/main/java/boombabob/rebalancingVanilla/mixin/OreDropsMixin.java
@@ -1,6 +1,5 @@
 package boombabob.rebalancingVanilla.mixin;
 
-import boombabob.rebalancingVanilla.RebalancingVanilla;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
